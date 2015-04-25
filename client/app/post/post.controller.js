319d97fbe6f66ab51e971dd6049627fa0dd464be
Berkecz Ádám <adam.berkecz@me.com>
2015-04-25 17:07:12 +0200

    Users can now post and read comments

--- client/app/post/post.controller.js
+++ client/app/post/post.controller.js
@@ -1,10 +1,31 @@
 'use strict';
 
 angular.module('tlogApp')
-  .controller('PostCtrl', function ($scope, $stateParams, $http, $resource, Post) {
+  .controller('PostCtrl', function ($scope, $stateParams, $http, $resource, Post, Comment, User) {
 
     Post.get({id: $stateParams.id}, function(result) {
       $scope.post = result;
     });
 
+    User.get(function(result) {
+      $scope.userId = result._id;
+    });
+
+    $scope.queryComments = function() {
+      Comment.query({postid: $stateParams.id}, function(result) {
+        $scope.comments = result;
+      });
+    }
+
+    $scope.queryComments();
+
+    $scope.post_comment = function() {
+      Comment.save({postid: $stateParams.id}, {content: $scope.commentToPost, author: $scope.userId}, function(success) {
+        console.log("success");
+        $scope.queryComments();
+        delete $scope.commentToPost;
+      }, function(error) {
+        console.error(error);
+      });
+    };
   });
--- client/app/post/post.service.js
+++ client/app/post/post.service.js
@@ -3,4 +3,6 @@
 angular.module('tlogApp')
   .service('Post', function ($resource) {
     return $resource('/api/posts/:id', {id: '@id'});
+  }).service('Comment', function($resource) {
+    return $resource('/api/posts/:postid/comments/:id', {postid: '@postid', id: '@id'});
   });
