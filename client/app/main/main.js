d91a01ea44080435419942b868e79387914e23da
kzs <katona.zsolt@tigra.hu>
2015-04-25 13:16:22 +0200

    initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('tlogApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
