177a6c5df64fd7dcbf76686e24cb458a26098499
Zsolt <zsolt@KZS-Mac.local>
2015-05-01 16:44:55 +0200

    trim unused code

--- server/routes.js
+++ server/routes.js
@@ -11,7 +11,6 @@ module.exports = function(app) {
   // Insert routes below
   app.use('/api/authorizations', require('./api/authorization'));
   app.use('/api/posts', require('./api/post'));
-  app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
 
   app.use('/auth', require('./auth'));
