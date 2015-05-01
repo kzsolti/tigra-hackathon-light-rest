b8fabbb69d18e97302a13f2bac79a283a5e75325
Zsolt <zsolt@KZS-Mac.local>
2015-05-01 20:14:31 +0200

    better seed data

--- server/config/seed.js
+++ server/config/seed.js
@@ -9,6 +9,57 @@ var User = require('../api/user/user.model');
 var Post = require('../api/post/post.model');
 var Comment = require('../api/comment/comment.model');
 
+var populateComments = function () {
+  Comment.find({}).remove(function () {
+    User.findOne({name: 'Test User'}, function (err, user) {
+      if (err) {
+        console.error(err);
+      } else {
+        Post.findOne({title: 'Welcome to TLog!'}, function (err, post) {
+          if (err) {
+            console.error(err);
+          } else {
+            Comment.create({
+              author: user._id,
+              post: post._id,
+              content: 'First!!!'
+            }, function () {
+              console.log('finished populating comments');
+            });
+          }
+        })
+      }
+    })
+  })
+};
+
+var populatePosts = function () {
+  User.findOne({name: 'Admin'}, function (err, admin) {
+    if (err) {
+      console.error(err);
+    } else {
+      Post.find({}).remove(function () {
+        Post.create({
+          author: admin._id,
+          title: 'Welcome to TLog!',
+          summary: 'Our new blog has opened up, come in and look around.',
+          content: 'This is our first blog post, so be sure read it and comment!',
+          createdOn: new Date()
+        }, {
+          author: admin._id,
+          title: 'Big news ahead!',
+          summary: 'TLog is now officially up and running.',
+          content: 'Become an author on this new and exciting blog site! Contact Admin to learn how.',
+          createdOn: new Date()
+        }, function () {
+          console.log('finished populating posts');
+          populateComments();
+        })
+      });
+    }
+  })
+};
+
 User.find({}).remove(function() {
   User.create({
     provider: 'local',
@@ -23,31 +74,6 @@ User.find({}).remove(function() {
     password: 'admin'
   }, function() {
     console.log('finished populating users');
+    populatePosts();
   });
-});
-User.find({}, function (err, users) {
-  console.log(users);
-});
-User.findOne({ name: 'Admin' }, function (err, admin) {
-  if (err) {
-    console.error(err);
-  } else {
-    Post.find({}).remove(function () {
-      Post.create({
-        //author: admin._id,
-        title: 'Welcome to TLog!',
-        summary: 'Our new blog has opened up, come in and look around.',
-        content: 'This is our first blog post, so be sure read it and comment!',
-        createdOn: new Date()
-      }, {
-        //author: admin._id,
-        title: 'Big news ahead!',
-        summary: 'TLog is now officially up and running.',
-        content: 'Become an author on this new and exciting blog site! Contact Admin to learn how.',
-        createdOn: new Date()
-      }, function () {
-        console.log('finished populating posts');
-      })
-    });
-  }
 });
