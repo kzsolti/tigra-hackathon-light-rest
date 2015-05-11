c14087f574d839d447ad925582de05981c96d33c
kzs <katona.zsolt@tigra.hu>
2015-05-11 18:40:29 +0200

    publish new post

--- client/app/post/create/createpost.controller.js
+++ client/app/post/create/createpost.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('tlogApp')
-  .controller('CreatePostCtrl', function ($scope, Auth, Post) {
+  .controller('CreatePostCtrl', function ($scope, $location, Auth, Post) {
 
     $scope.ckOptions = {
       language: 'en',
@@ -9,5 +9,16 @@ angular.module('tlogApp')
     };
 
     $scope.newPost = {};
+
+    $scope.publish = function () {
+      $scope.newPost.author = Auth.getCurrentUser()._id;
+      Post.save({}, $scope.newPost,
+        function (result) {
+          $location.path('/post/' + result._id);
+        }, function (error) {
+          console.error(error);
+        }
+      );
+    };
 
   });
